6421c0d8d9bd815fd6dcc7185c00c0d10e4abe0e
DaAxe <atossa@protonmail.com>
2020-03-02 13:49:28 +1000

    added side nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #Main Page
+  # Main Page
   root to: "home#index"
 
+  # Show Shop
   get "/shop", to: "shop#index", as: "shop"
 
+  # Show individual Item
+  get "/shop/:id", to: "shop#show", as: "item"
+
+  # Show member profile
+   
 end
